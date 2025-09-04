b0c92654f236be7ff9a4ff1ad7a6436d0d876ec0
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2025-09-04 11:32:10 +0000

    commited

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountEmail = "shahnawazahmad1050@gmail.com"
+var accountPassword = 790345
+accountCity = "patna"
+let accountState
+
+// accountId = 2
+/*
+do not use var 
+follow above line
+*/
+
+accountEmail ="badshah@gmail.com"
+accountPassword = 1050
+accountCity = "Bhosri"
+
+
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity,accountState])
