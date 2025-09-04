5f6509b154008da27b6bae0faecf39051324bd8b
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2025-09-04 15:02:22 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict";  // treat all JS code as a new version
+
+// alert("Shahanawaz") // wer are using node JS, not browser
+
+console.log(2
+    +
+    2) // code redablity should be 
+    
+    console.log("Hitesh")
+
+    let names = "shah"
+    let age = 25
+    let isLogedIn = false
+    let state;
+
+    // number => 2 to power 53
+    //bigInt
+    //String => ""
+    //Boolean => false/true
+    //null => Standalone value
+    //undefined => 
+    // Symbol => unique , we are going to use in react
+
+
+    //Object
+
+    console.log(typeof null) //Object
+    console.log(typeof names)// String
+    console.log(typeof isLogedIn)// Boolean
+    console.log(typeof undefined)// undefined
+
+
+
+
