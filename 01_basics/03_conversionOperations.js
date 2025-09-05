03452903970e6df7bb17a0231e53e7c8b2e26e27
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2025-09-05 17:28:39 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = "hitesh"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+//console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+
+// "22" => 22
+// "22bc" => NaN
+// true => 1, false => 0
+
+let isLoggedin = "shah"
+
+let booleanLoggeddIn = Boolean(isLoggedin)
+
+//console.log(booleanLoggeddIn)
+//console.log(typeof booleanLoggeddIn)
+
+// 1 => true, 0 = false
+// "" => false
+// "shah" => true
+
+
+let someNumbr = 22
+
+let stringNumber = String(someNumbr)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
