121c051cb4047ced340f7f8e5cabcef71211ca37
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2025-09-06 04:19:38 +0000

    opertaions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -28,7 +28,57 @@ let someNumbr = 22
 
 let stringNumber = String(someNumbr)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// **************Operations*******************
+
+let value = 4
+
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(3+3)
+// console.log(3-3)
+// console.log(3*3)
+// console.log(3**3) // power like 2 raise to the power 1
+// console.log(3/3)
+// console.log(3%3)
 
 
+let str1 ="Shahnawaz"
+let str2 = " Ahmad"
+ let str3 = str1 + str2 
+
+//  console.log(str3)
+
+
+//  console.log(1 + "2")
+//  console.log("1" + 2)
+
+//  console.log("1" +2 + 2)
+//  console.log(1 + 2 + "2")
+
+//  console.log(3 * 5 + 2 % 5) // not a good way to code, less redablity
+
+// console.log(true)
+// console.log(+true)
+// //console.log(true+)
+
+// console.log(+"")
+// console.log(-true)
+
+let num1 , num2 , num3 , num4 ;
+
+ num1 = num2 = num3 = num4 = 2+2
+
+ let gameCounter = 100
+
+ gameCounter++
+
+ console.log(gameCounter)
+
+ ++gameCounter
+
+ console.log(gameCounter)
