4c01bfc526245bbf6bff817ff0618b89b87cfe2f
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2025-09-07 14:55:02 +0000

    commit

---
+++ 01_basics/dataype-summary.js
@@ -0,0 +1,26 @@
+//Primitive
+
+//7 types: String , Number, Booelan , Null ,Undefined, Symbol, BigInt
+
+
+const id = Symbol('1235')
+
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+//Refrences (Non-Primitve)
+// arrays, Object , Function
+
+let heros = ["SUperman, Batman, Ironman, Shaktiman"]
+
+const myObj = {
+    name: "Shahnawaz",
+    age: "26"
+}
+
+const myFunction = function(){
+    console.log("Shahnawaz")
+}
+ 
+
+
