873f07b036e15226d998d6e6ff83864a0d7860ec
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2025-09-07 15:12:57 +0000

    commit

--- 01_basics/dataype-summary.js
+++ 01_basics/dataype-summary.js
@@ -3,24 +3,49 @@
 //7 types: String , Number, Booelan , Null ,Undefined, Symbol, BigInt
 
 
-const id = Symbol('1235')
+// const id = Symbol('1235')
 
-const anotherId = Symbol('123')
-console.log(id === anotherId)
+// const anotherId = Symbol('123')
+// console.log(id === anotherId)
 
-//Refrences (Non-Primitve)
-// arrays, Object , Function
+// //Refrences (Non-Primitve)
+// // arrays, Object , Function
 
-let heros = ["SUperman, Batman, Ironman, Shaktiman"]
+// let heros = ["SUperman, Batman, Ironman, Shaktiman"]
 
-const myObj = {
-    name: "Shahnawaz",
-    age: "26"
-}
+// const myObj = {
+//     name: "Shahnawaz",
+//     age: "26"
+// }
 
-const myFunction = function(){
-    console.log("Shahnawaz")
-}
+// const myFunction = function(){
+//     console.log("Shahnawaz")
+// }
  
 
+//  *****************************************************************
 
+//Memory
+
+// Stack(Primitve)          Heap(Non-Primitve)
+
+let names = "shahnawaz"
+let anotherNames = names
+
+anotherNames = "Walker"
+
+console.log(names)
+console.log(anotherNames)
+
+
+let userOne = {
+     name:"Shah",
+     sirname:"ahmad"    
+}
+
+let userTwo = userOne
+
+userTwo.name = "nawaz"
+
+console.log(userOne.name)
+console.log(userTwo.name)
